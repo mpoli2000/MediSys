5b9dd913789fb0f90ddfaedcc10f897c3fe10744
mateo <mateopolicichio@gmail.com>
2022-06-10 18:57:58 -0300

    Medico

--- src/com/medisys/entidades/Medico.java
+++ src/com/medisys/entidades/Medico.java
@@ -1,8 +1,14 @@
-package com.medisys;
+package com.medisys.entidades;
 
 public class Medico {
+    int id_medico;
     String nombre;
     String apellido;
+    String email;
+    String clave;
+    int dni;
     int matricula;
     String especialidad;
+
+
 }
